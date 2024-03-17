d9c396ee91a5ade52eef51c2808df41ff864501d
Stephen Park <ssp0929@gmail.com>
2024-03-16 23:38:08 -0700

    Delete deprecated file

--- src/libs/redisStore.ts
+++
@@ -1,9 +0,0 @@
-import RedisStore from 'connect-redis'
-import redis from './ioredis'
-
-const redisStore = new RedisStore({
-  client: redis,
-  prefix: 'session:',
-})
-
-export default redisStore
